bfa54252be72c2f880efd4cb3de26413c679e92d
ninazmul <nazmulsaw@gmail.com>
2024-07-16 16:07:07 +0600

    will work frontend later

---
+++ client/app/utils/Heading.tsx
@@ -0,0 +1,15 @@
+
+interface HeadingProps{
+    title: string;
+    description: string;
+    keywords: string;
+}
+const Heading = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Heading
