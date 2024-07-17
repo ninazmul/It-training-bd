8648820937048e1889a93aec86f0b14d552d9ae2
ninazmul <nazmulsaw@gmail.com>
2024-07-17 17:11:01 +0600

    place order and get notification and email done

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import ErrorMiddleware from "./middleware/error";
 import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
+import orderRouter from "./routes/order.route";
 
 require("dotenv").config();
 
@@ -23,9 +24,7 @@ app.use(
 );
 
 // routes
-app.use("/api/v1", userRouter);
-
-app.use("/api/v1", courseRouter);
+app.use("/api/v1", userRouter, courseRouter, orderRouter);
 
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
