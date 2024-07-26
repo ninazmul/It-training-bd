0f5bccdf211979a599d7fe048bb2dab16683c3c7
ninazmul <nazmulsaw@gmail.com>
2024-07-26 11:52:20 +0600

    Navbar and Theme Switcher done

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,18 +1,27 @@
-"use client"
+"use client";
 
-import { FC } from "react"
+import { FC, useState } from "react";
+import Heading from "./utils/Heading";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
-interface PageProps{
-
-}
+interface PageProps {}
 
 const Page: FC<PageProps> = (PageProps) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+
   return (
     <div>
-      
+      <Heading
+        title="IT Training BD"
+        description="IT Training BD is a platform for students to learn and get help from teachers"
+        keywords="Programming, MERN, Redux, Machine Learning, Course"
+      />
+      <Header open={open} setOpen={setOpen} activeItem={activeItem} />
+      <Hero/>
     </div>
-  )
-}
+  );
+};
 
-export default Page
-
+export default Page;
--- client/app/utils/Heading.tsx
+++ client/app/utils/Heading.tsx
@@ -1,15 +1,19 @@
+import { FC } from "react";
 
-interface HeadingProps{
-    title: string;
-    description: string;
-    keywords: string;
+interface HeadingProps {
+  title: string;
+  description: string;
+  keywords: string;
 }
-const Heading = () => {
+const Heading: FC<HeadingProps> = ({ title, description, keywords }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <title>{title}</title>
+      <meta name="viewport" content="width-device-width, initial-scale=1" />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+    </>
+  );
+};
 
-export default Heading
+export default Heading;
