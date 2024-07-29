8308e0f316a33f75e9a058617e22c9a4def84b2c
ninazmul <nazmulsaw@gmail.com>
2024-07-29 10:01:47 +0600

    Loader done

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import "./globals.css";
 import { Poppins, Josefin_Sans } from "next/font/google";
@@ -6,6 +6,9 @@ import { ThemeProvider } from "./utils/theme-provider";
 import { Toaster } from "react-hot-toast";
 import { Providers } from "./Provider";
 import { SessionProvider } from "next-auth/react";
+import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
+import { useEffect } from "react";
+import Loader from "./components/Loader/Loader";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -18,11 +21,7 @@ const josefin = Josefin_Sans({
   variable: "--font-Josefin",
 });
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body
@@ -31,7 +30,7 @@ export default function RootLayout({
         <Providers>
           <SessionProvider>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-              {children}
+              <Custom>{children}</Custom>
               <Toaster position="top-center" reverseOrder={false} />
             </ThemeProvider>
           </SessionProvider>
@@ -40,3 +39,9 @@ export default function RootLayout({
     </html>
   );
 }
+
+const Custom: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { isLoading } = useLoadUserQuery({});
+
+  return <>{isLoading ? <Loader /> : <>{children}</>}</>;
+};
