2d0e5eb400b486d31a6ea9fb3dfc010d6835e973
ninazmul <nazmulsaw@gmail.com>
2024-08-06 12:01:00 +0600

    add remove categories done

---
+++ client/app/admin/categories/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import EditCategories from "@/app/components/Admin/customization/EditCategories";
+import DashboardHero from "@/app/components/Admin/DashboardHero";
+import AdminSidebar from "@/app/components/Admin/sidebar/AdminSidebar";
+import Heading from "@/app/utils/Heading";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return (
+    <div>
+      <Heading
+        title="LMS - Admin"
+        description="LMS is a platform for students to learn and get help from teachers"
+        keywords="Programming, MERN, Redux, Machine Learning"
+      />
+      <div className="flex h-min-screen">
+        <div className="1500px:w-[16%] w-1/5">
+          <AdminSidebar />
+        </div>
+        <div className="w-[85%]">
+          <DashboardHero />
+          <EditCategories />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
