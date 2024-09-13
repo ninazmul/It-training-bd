3fdda6093eef0229794af44a023a2bfb250ae2cc
ninazmul <nazmulsaw@gmail.com>
2024-09-13 12:09:01 +0600

    updated

--- server/app.ts
+++ server/app.ts
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(
   cors({
     // origin: process.env.ORIGIN,
-    origin: ["http://localhost:3000"],
+    origin: ["https://it-server.up.railway.app"],
     credentials: true,
   })
 );
